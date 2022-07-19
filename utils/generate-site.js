582fee021a614a634faf51c0bd954e5389a1928c
KyleAxley <kanderson7645@yahoo.com>
2022-07-18 21:33:18 -0400

    minor edits

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
-const writeFile = (fileContent) => {
+const writeFile = fileContent => {
   return new Promise((resolve, reject) => {
-    fs.writeFile("./dist/index.html", fileContent, (err) => {
+    fs.writeFile("./dist/index.html", fileContent, err => {
       //if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
       if (err) {
         reject(err);
@@ -19,9 +19,9 @@ const writeFile = (fileContent) => {
   });
 };
 
-const copyFile = (fileContent) => {
+const copyFile = () => {
   return new Promise((resolve, reject) => {
-    fs.copyFile("./src/style.css", "./dist/style.css", fileContent, (err) => {
+    fs.copyFile("./src/style.css", "./dist/style.css", err => {
       if (err) {
         reject(err);
         return;
